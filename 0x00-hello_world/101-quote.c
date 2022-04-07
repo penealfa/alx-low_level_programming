96edf620b704f76447d121a9eeb32574630d3183
penealfa <penealfeleke17@gmail.com>
2022-04-07 12:12:01 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *quote task printing to stderr task
+ * Return: return 1 after program execution
+ */
+int main(void)
+{
+	       fprintf( stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",30);
+		        return (1);
+}
