5bfd118122a24f02b2e40122100c0477efd5659d
penealfa <penealfeleke17@gmail.com>
2022-04-07 12:16:31 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - Entry point
- *quote task printing to stderr task
- * Return: return 1 after program execution
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	       fprintf( stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",30);
-		        return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
